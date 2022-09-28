cf78c026855f20ea4073aecf7d45d687582f655e
James Peterson <jamespetersonwrestling@gmail.com>
2022-09-28 01:44:41 -0600

    Milestone 1 copmlete. It took 1 hour & 45 minutes.

--- WordleStarter copy/Wordle.py
+++ WordleStarter copy/Wordle.py
@@ -11,12 +11,20 @@ from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
 def wordle():
+    
 
     def enter_action(s):
         gw.show_message("You have to implement this method.")
-
+    
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
+
+    RandWord = FIVE_LETTER_WORDS[random.randrange(0,len(FIVE_LETTER_WORDS),1)]
+    print(RandWord)
+    for i in range(5):
+        gw.set_square_letter(0, i, RandWord[i])
+
+
 
 # Startup code
 
